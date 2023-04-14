328d4da11a1722bec4668946bb36fd7d62f8a03a
MyatMinAung <iridiamthegreat@gmail.com>
2023-04-14 13:20:09 +0630

    fix: fontSize at preloader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -10,23 +10,20 @@ const Loader = ({ children }) => {
 
         const tl = gsap.timeline({ defaults: { duration: 0.7 } });
         tl.fromTo(q(".loading-text"), { y: 120 }, { y: -10 });
-        tl.to(
-            q(".white-bg"), { x: "-100%" }).to(
-            q(".dark-bg"),{ x: "-100%", duration: 0.6 },
-            "-=0.6"
-        );
+        tl.to(q(".white-bg"), { x: "-100%" })
+        .to(q(".dark-bg"),{ x: "-100%", duration: 0.6 }, "-=0.6");
     }, []);
 
     return (
         <div ref={loadingRef} aria-hidden="true">
             <div className="white-bg fixed top-0 left-0 w-full h-screen bg-[#1c1e28] z-[9999] flex justify-center items-center">
                 <div className="overflow-hidden">
-                    <span className="loading-text text-primaryColor inline-block text-bgdark dark:text-bglight text-4xl sm:text-5xl lg:text-7xl tracking-widest">
+                    <span className="loading-text uppercase text-primaryColor inline-block text-2xl sm:text-4xl tracking-widest">
                         {children}
                     </span>
                 </div>
             </div>
-            <div className="dark-bg fixed top-0 left-0 w-full h-screen bg-primaryColor dark:bg-carrigreen z-[9998]"></div>
+            <div className="dark-bg fixed top-0 left-0 w-full h-screen bg-primaryColor z-[9998]"></div>
         </div>
     );
 }
