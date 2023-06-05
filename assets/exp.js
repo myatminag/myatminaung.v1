74b16ed0d9bbb0974e77b1d610ea61d60d38c6c2
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-05 21:39:53 +0630

    Fix: exp data

--- assets/exp.js
+++ assets/exp.js
@@ -1,6 +1,6 @@
 export const expData = [
     {
-        jobTitle: "Junior Full-stack Developer",
+        jobTitle: "Junior Full-Stack Developer Volunteer",
         companyName: "Tee Htwin",
         date: "November 2022 - Present (Part Time)",
         description: [
@@ -16,9 +16,9 @@ export const expData = [
         companyName: "Shopdoora",
         date: "January 2023 - Present (Full Time)",
         description: [
-            "Collaborated with cross-functional teams, including designers, testers, and backend developers, to deliver high-quality features on tight timelines",
-            "Developed and maintained micro-frontends using React, Next and state managment libraries like Redux and Zustand.",
-            "Implemented responsive design strategies and mobile-first development approach to ensure seamless user experiences across all devices",
+            "Collaborated with cross-functional teams, including designers, testers, and backend developers, to deliver high-quality features on tight timelines.",
+            "Developed and maintained micro-frontends using React, Next and state management libraries like Redux and Zustand.",
+            "Implemented responsive design strategies and mobile-first development approach to ensure seamless user experiences across all devices.",
             "Worked alongside with five front-end developers for developing eCommerce service platforms for variety of seller, retailer, and vendor.",
             "Implemented and shipped the front-end of Aya Zay multi-seller web app for Aya Bank which let users can buy various kinds of products at Aya Pay application.",
         ],
