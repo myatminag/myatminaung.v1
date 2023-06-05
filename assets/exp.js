c05716a5ef63dff96b0a131f45bbff05f435c6c2
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-05 21:21:24 +0630

    Update: exp data

--- assets/exp.js
+++ assets/exp.js
@@ -1,14 +1,26 @@
 export const expData = [
     {
+        jobTitle: "Junior Full-stack Developer",
+        companyName: "Tee Htwin",
+        date: "November 2022 - Present (Part Time)",
+        description: [
+            "Collaborated with a team of developers and designers to create responsive and user-friendly web applications.",
+            "Implemented front-end frameworks such as React and Nextjs to enhance the user interface and improve overall user experience.",
+            "Utilized server-side programming languages like Node.js and Express.js to build robust and scalable back-end functionality.",
+            "Collaborated closely with team members to understand project requirements, provide feedback, and deliver non-profit projects.",
+            "Kept up-to-date with the latest technologies, industry trends, and best practices through continuous learning and self-improvement.",
+        ],
+    },
+    {
         jobTitle: "Junior Front-End Developer",
         companyName: "Shopdoora",
-        date: "January 2023 - Present",
+        date: "January 2023 - Present (Full Time)",
         description: [
             "Collaborated with cross-functional teams, including designers, testers, and backend developers, to deliver high-quality features on tight timelines",
             "Developed and maintained micro-frontends using React, Next and state managment libraries like Redux and Zustand.",
             "Implemented responsive design strategies and mobile-first development approach to ensure seamless user experiences across all devices",
             "Worked alongside with five front-end developers for developing eCommerce service platforms for variety of seller, retailer, and vendor.",
-            "Implemented and shipped the front-end of Aya Zay multi-seller web app for Aya Bank which let users can buy various kinds of products at Aya Pay application."
-        ]
+            "Implemented and shipped the front-end of Aya Zay multi-seller web app for Aya Bank which let users can buy various kinds of products at Aya Pay application.",
+        ],
     },
 ];
