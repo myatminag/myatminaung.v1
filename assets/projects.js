3ec975cc698654ce513ec20de2143c6aa1b7bc07
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-06 20:12:16 +0630

    Update: projects data

--- assets/projects.js
+++ assets/projects.js
@@ -2,25 +2,43 @@ export const Projects = [
     {
         id: 1,
         imageIsLeft: 1,
+        imgUrl: "https://res.cloudinary.com/dxvclcibh/image/upload/v1686058244/photo_2023-06-06_20-00-05_o0lxkx.jpg",
+        title: "Bitbazzar",
+        description:
+            "BitBazaar offers an extensive and diverse collection of games and also have a wide range of games available, so you can find the perfect fit for your gaming setup.",
+        demoLink: "https://bitbazaar.vercel.app/",
+        githubLink: "https://github.com/MyatminAung2001/BitBazaar",
+        stacks: [
+            { id: 1, stackOne: "Nextjs" },
+            { id: 2, stackOne: "TailwindCss" },
+            { id: 3, stackOne: "ReactQuery" },
+            { id: 4, stackOne: "Rawg.io Api" },
+        ],
+    },
+    {
+        id: 2,
+        imageIsLeft: 0,
         imgUrl: "https://res.cloudinary.com/dqscrfky2/image/upload/v1681562399/Mockup_2_rubh08.png",
         title: "Empress Store",
-        description: "A fully functional ecommerce built with mern stack that users can browse variety of products and also allows for secure online transaction using the integrated stripe gateway.",
+        description:
+            "A fully functional ecommerce built with mern stack that users can browse variety of products and also allows for secure online transaction using the integrated stripe gateway.",
         demoLink: "https://empress.vercel.app",
         githubLink: "https://github.com/MyatminAung2001/Empress-Frontend",
         stacks: [
             { id: 1, stackOne: "Reactjs" },
             { id: 2, stackOne: "TailwindCss" },
             { id: 3, stackOne: "Nodejs" },
-            { id: 5, stackOne: "Expressjs" },
+            { id: 4, stackOne: "Expressjs" },
             { id: 5, stackOne: "Mongodb" },
-        ]
+        ],
     },
     {
-        id: 2,
-        imageIsLeft: 0,
+        id: 3,
+        imageIsLeft: 1,
         imgUrl: "https://res.cloudinary.com/dqscrfky2/image/upload/v1681562410/Phone_Mac_Mockup_eungyh.png",
         title: "Journey Blog",
-        description: "This blog website is inspired by The collector website. It is supported dark & light mode and users can exlpore different kinds of articles on history, art and philosophy.",
+        description:
+            "This blog website is inspired by The collector website. It is supported dark & light mode and users can exlpore different kinds of articles on history, art and philosophy.",
         demoLink: "https://journey-blog-flax.vercel.app",
         stacks: [
             { id: 1, stackOne: "Reactjs" },
@@ -29,14 +47,15 @@ export const Projects = [
             { id: 4, stackOne: "Nodejs" },
             { id: 5, stackOne: "Expressjs" },
             { id: 6, stackOne: "Mongodb" },
-        ]
+        ],
     },
     {
-        id: 3,
-        imageIsLeft: 1,
+        id: 4,
+        imageIsLeft: 0,
         imgUrl: "https://res.cloudinary.com/dqscrfky2/image/upload/v1681562420/Mockup_3_vwmvwu.png",
         title: "Authors Panel",
-        description: "This is the author panel for journey blog where authors can publish or update their post. It can also approve membership for joining community.",
+        description:
+            "This is the author panel for journey blog where authors can publish or update their post. It can also approve membership for joining community.",
         demoLink: "https://journey-panel.vercel.app",
         stacks: [
             { id: 1, stackOne: "Reactjs" },
@@ -45,6 +64,6 @@ export const Projects = [
             { id: 4, stackOne: "Nodejs" },
             { id: 5, stackOne: "Expressjs" },
             { id: 6, stackOne: "Mongodb" },
-        ]
-    }
-]
+        ],
+    },
+];
